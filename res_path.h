248e852102d0d72998083e753f35d58ff40ba2e0
Mateusz Wojtczak <xmateusz.wojtczakx@gmail.com>
2023-02-13 18:35:48 +0100

    zmiant dotyczące review

--- res_path.h
+++ res_path.h
@@ -17,27 +17,23 @@
  * Paths returned will be Lessons/res/subDir
  */
 std::string getResourcePath(const std::string &subDir = ""){
-	//We need to choose the path separator properly based on which
-
-	//platform we're running on, since Windows uses a different
-
-	//separator than most systems
+   /*  We need to choose the path separator properly based on which
+	*  platform we're running on, since Windows uses a different
+	*  separator than most systems
+	*/
 
 #ifdef _WIN32
 	const char PATH_SEP = '\\';
 #else
 	const char PATH_SEP = '/';
 #endif
-	//This will hold the base resource path: Lessons/res/
-
-	//We give it static lifetime so that we'll only need to call
-
-	//SDL_GetBasePath once to get the executable path
-
+	/* This will hold the base resource path: Lessons/res/
+	*  We give it static lifetime so that we'll only need to call
+	*  SDL_GetBasePath once to get the executable path
+	*/
 	static std::string baseRes;
 	if (baseRes.empty()){
 		//SDL_GetBasePath will return NULL if something went wrong in retrieving the path
-
 		char *basePath = SDL_GetBasePath();
 		if (basePath){
 			baseRes = basePath;
@@ -48,14 +44,12 @@ std::string getResourcePath(const std::string &subDir = ""){
 			return "";
 		}
 		//We replace the last bin/ with res/ to get the the resource path
-
 		size_t pos = baseRes.rfind("bin");
 		baseRes = baseRes.substr(0, pos) + "res" + PATH_SEP;
 	}
-	//If we want a specific subdirectory path in the resource directory
-
-	//append it to the base path. This would be something like Lessons/res/Lesson0
-
+	/* If we want a specific subdirectory path in the resource directory
+	* append it to the base path. This would be something like Lessons/res/Lesson0
+	*/
 	return subDir.empty() ? baseRes : baseRes + subDir + PATH_SEP;
 }
 
